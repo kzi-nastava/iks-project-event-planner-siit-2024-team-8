7a8e13042f9577aa31c9f87ec9f970171ada2c6f
Viktor Srbljin <viktor.srbljin@gmail.com>
2024-11-19 22:58:16 +0100

    feat: added deletion popup

---
+++ src/app/dialogs/delete-confirmation-dialog/delete-confirmation-dialog.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-confirmation-dialog',
+  templateUrl: './delete-confirmation-dialog.component.html',
+  styleUrls: ['./delete-confirmation-dialog.component.css']
+})
+export class DeleteConfirmationDialogComponent {}
